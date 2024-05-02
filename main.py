a7aded68e01c04b626c893a964bffac783904156
Ammar Syed Ali <ammarali0416@gmail.com>
2024-05-02 00:21:31 -0400

    - added metadata to the API docs

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from dotenv import load_dotenv, find_dotenv
 
 from Schemas import LocationCount
 from google_sheets import append_row
+import metadata
 
 load_dotenv(find_dotenv())
 
-app = FastAPI()
+app = FastAPI(
+    title=metadata.title,
+    description=metadata.description,
+    summary=metadata.summary,
+    version=metadata.version,
+    contact=metadata.contact
+)
 
 origins = [
     "*"
@@ -22,7 +29,12 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.post("/AddCount", status_code=status.HTTP_202_ACCEPTED, summary="Add a count to the Google Sheet", response_model=LocationCount, response_description="The record added to the Google Sheet")
+@app.post("/AddCount", 
+          status_code=status.HTTP_202_ACCEPTED, 
+          summary="Add a count to the Google Sheet", 
+          response_model=LocationCount, 
+          response_description="The record added to the Google Sheet",
+          tags=["Data Collection"])
 async def add_count(count: LocationCount) -> LocationCount:
     response = await append_row(count)
     return response
---
+++ metadata.py
@@ -0,0 +1,21 @@
+title = 'Project Downtown Tampa Backend'
+summary = 'This is the backend for the Project Downtown Tampa App.'
+description = """
+All thanks to the Almighty
+
+This backend adds location data to the PDT Google Sheet.
+Future version will support additional functionality.
+
+## Current Features
+* Add a record to the data collection Google Sheet
+
+## Future Features
+* Add image and audio data to the Google Sheet/Drive
+* Data validation (whaterver that means lol)
+
+"""
+version = '0.1'
+contact = {
+    'Name': 'Ammar Syed Ali',
+    'email': 'ammarali0416@gmail.com'
+}
